cf9b1a9966660893e66fbae58d8e4650c5dc74a5
dejavuog <37196535+dejavuog@users.noreply.github.com>
2018-05-15 14:55:24 +0300

    Add files via upload

---
+++ botforbeb.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_member_join(user=discord.Member):
+    
+    global bot
+
+    channel = discord.Object('445125257062252564')
+    server = user.server
+    memer = discord.utils.get(server.roles, name="Regular Cunts (New niggas that should be welcomed with a tit pic (in dm of course) ok?)",)
+    await bot.add_roles(user, memer)
+    msg = "Alright {} listen up. You look like a fun guy which is why it'd be a shame to have to ban you so just go fucking read <#445135004826206209> and <#445135060702593025> you utter cunt and you won't wake up in a Taliban prison camp."
+    await bot.send_message(channel, msg)
+
+@bot.command()
+@commands.has_role("Super Sexy Admins (Suck them off if you want to stay in this server)")
+async def say(*, message:str):
+    await bot.send_message(discord.Object('445125257062252564'), content=message)
+
+
+@bot.command()
+@commands.has_role("Super Sexy Admins (Suck them off if you want to stay in this server)")
+@commands.cooldown(1, 30, commands.BucketType.user)
+async def spam(times : int, *,content:str):
+    if times > 10:
+        await bot.say("i can only spam something 10 times nigger boi")
+    else:
+        for i in range(times):
+            await bot.say(content)
+
+bot.run('NDQ1MTQ4MzE1MjAyNjE3MzQ0.DdsfCA.984mb9zb_nxHPpEOd2ekqrkGOzA')
